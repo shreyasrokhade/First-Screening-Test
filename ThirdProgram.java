deab2babb854e8f769ac308f06bb846333f21c7b
Shreyas231 <95679541+shreyasrokhade@users.noreply.github.com>
2023-07-17 17:10:42 +0530

    Update ThirdProgram.java

--- ThirdProgram.java
+++ ThirdProgram.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
+
+public class OddNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = scanner.nextInt();
+        int[] oddNumbers = new int[a];
+        if(a%2==0){
+            int n=a-1;
+            int i=1;
+            for(int j=0;j<n;j++){
+                oddNumbers[j]=i;
+                i=i+2;
+            }
+            for(int j=0;j<n;j++){
+                System.out.print(oddNumbers[j]+ " ");
+            }
+        }
+        else{
+        int i = 1;
+        for (int j = 0; j < a; j++) {
+            oddNumbers[j] = i;
+            i = i + 2;
+        }
+        for (int j = 0; j < a; j++) {
+            System.out.print(oddNumbers[j] + " ");
+        }
+    }
+}
+}
 
