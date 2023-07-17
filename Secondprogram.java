732a70b606783ffd2ee94f1f17adcba0333520bf
Shreyas231 <95679541+shreyasrokhade@users.noreply.github.com>
2023-07-17 16:19:52 +0530

    Update Secondprogram.java

--- Secondprogram.java
+++ Secondprogram.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class OddNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        int a = scanner.nextInt();
+        int[] oddNumbers = new int[a];
+        int i = 1;
+        for (int j = 0; j < a; j++) {
+            oddNumbers[j] = i;
+            i = i + 2;
+        }
+        for (int j = 0; j < a; j++) {
+            System.out.print(oddNumbers[j] + " ");
+        }
+    }
+}
